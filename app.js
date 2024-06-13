90aa737895f71ea4efa9169c8ba2507c9262e7b0
DBEST-EZRA <ezradbest101@gmail.com>
2024-06-13 12:34:38 +0300

    my company website

---
+++ app.js
@@ -0,0 +1,24 @@
+const menuToggle = document.getElementById("mobile-menu");
+const navLinks = document.getElementById("nav-links");
+
+const boxes = document.querySelectorAll(".box");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  menuToggle.querySelector("i").classList.toggle("fa-bars");
+  menuToggle.querySelector("i").classList.toggle("fa-times");
+});
+
+window.addEventListener("scroll", checkBoxes);
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
